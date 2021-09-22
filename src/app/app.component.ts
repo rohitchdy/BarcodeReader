122c3e3ae391714c4fd666c014171282a795a2b3
Rohit Chaudhary <rohit.chaudhary@imark.com.np>
2021-09-22 23:33:53 +0545

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { BarcodeScannerLivestreamComponent, BarcodeScannerLivestreamOverlayComponent, BarcodeScannerLivestreamOverlayModule } from 'ngx-barcode-scanner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'BarcodeAndQRCodeScanner';
+export class AppComponent implements AfterViewInit {
+  // title = 'BarcodeAndQRCodeScanner';
+  @ViewChild(BarcodeScannerLivestreamComponent)
+  barcodeScanner!: BarcodeScannerLivestreamComponent;
+  barcodeValue:any;
+  ngAfterViewInit(){
+    this.barcodeScanner.start();
+  }
+  onValueChanges(result:any){
+    this.barcodeValue=result.codeResult.code;
+  }
+  onStarted(){
+    console.log('started');
+  }
 }
